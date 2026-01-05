09db69a5cad839994eda3fc1d23b3e6329928143
Diya Kothari <diyadkothari@gmail.com>
2026-01-05 14:02:41 +0530

    Fix: Change EventCard to default import in events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,4 +1,4 @@
-import { EventCard } from '@/components/EventCard';
+import EventCard from '@/components/EventCard';
 import { events } from '@/data/events';
 
 export default function EventsPage() {
