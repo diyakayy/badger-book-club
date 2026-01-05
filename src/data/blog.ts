5c2a9684e6d9ca23c5b38a0dc618bed389dac72f
Diya Kothari <diyadkothari@gmail.com>
2026-01-05 11:29:16 +0530

    Create blog.ts

---
+++ src/data/blog.ts
@@ -0,0 +1,23 @@
+export const blogPosts = [
+  {
+    id: 'why-read-classics',
+    title: 'Why We Should Still Read Classic Literature',
+    excerpt: 'Exploring timeless themes in classic books.',
+    date: '2025-01-10',
+    author: 'Sarah Mitchell',
+  },
+  {
+    id: 'book-club-benefits',
+    title: 'The Hidden Benefits of Joining a Book Club',
+    excerpt: 'Discover how reading together strengthens friendships.',
+    date: '2025-01-05',
+    author: 'James Chen',
+  },
+  {
+    id: 'discovering-new-authors',
+    title: 'How to Discover New Authors You\'ll Love',
+    excerpt: 'Tips and tricks for finding your next favorite writer.',
+    date: '2024-12-28',
+    author: 'Emma Davis',
+  },
+];
